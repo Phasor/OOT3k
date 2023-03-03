9ebeddddf1ab23254d3ca3ecc043adbddbe3b261
ben <phas0ruk@gmail.com>
2023-03-02 17:11:14 -0800

    adds hero

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -3,13 +3,20 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <div className='w-full absolute top-0 left-0 right-0'>
-        <Image
-            src="/banner2.jpeg"
-            alt="Picture of Oceans of Terra"
-            width={1920}
-            height={1080}
-        />
+    // <div className="w-full h-0 absolute bg-no-repeat inset-0 bg-cover bg-center md:bg-top  border-b-4 border-black " style={{backgroundImage: 'url(/banner2.jpeg)', paddingTop: '56.52%'}}>
+    //     <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-hero text-8xl text-gray-900'>Oceans of Terra</p>
+    // </div>
+    <div className="w-full h-0 absolute bg-no-repeat inset-0 bg-cover bg-center md:bg-top  border-b-4 border-black " style={{backgroundImage: 'url(/banner2.jpeg)', paddingTop: '56.52%'}}>
+        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-hero text-8xl text-gray-900" style={{top: '20%'}}>
+            Oceans
+        </p>
+        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-hero text-8xl text-gray-900" style={{top: '30%'}}>
+            of
+        </p>
+        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-hero text-8xl text-gray-900" style={{top: '40%'}}>
+            Terra
+        </p>
     </div>
+
   )
 }
