64eae81ba4250ef9da897d7514b900e61bd32ffb
ben <phas0ruk@gmail.com>
2023-03-03 11:06:05 -0800

    adds mermaid

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -3,12 +3,24 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-        <div className="w-full h-screen flex justify-center items-center bg-[#74acf7]">
+        <div className="w-full h-screen flex justify-center items-center bg-[#74acf7] relative">
             <div className='text-center'>
                 <p className='font-hero text-[8rem] text-white drop-shadow-xl'>Oceans</p>
                 <p className='font-hero text-[4rem] text-white drop-shadow-xl'>of</p>
                 <p className='font-hero text-[8rem] text-white drop-shadow-xl'>Terror</p>
             </div>
+            <Image 
+              src="/mermaid.png"
+              alt="Hero Image"
+              width={500}
+              height={500}
+              className="absolute bottom-0 right-0"
+            />
+            <div class="custom-shape-divider-bottom-1677869480" className="z-50">
+              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+          </div>
         </div>
   )
 }
--- frontend/components/Intro.js
+++ frontend/components/Intro.js
@@ -2,20 +2,16 @@ import React from 'react'
 
 export default function Intro() {
   return (
-        <div className='w-full h-screen bg-[#4b1fcd] flex relative'>
-            <div class="custom-shape-divider-top-1677866635">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-                </svg>
-            </div>
+        <div className='w-full h-screen bg-[#4b1fcd] flex '>
+    
 
-            <div className='mx-auto p-4 flex justify-center space-x-20 w-[70%] border border-yellow-500'>
-                <div className='text-center p-2 border rounded-lg shadow-lg flex flex-col justify-center items-center'>
+            <div className='mx-auto p-4 flex justify-center space-x-20 w-[70%]'>
+                <div className='text-center p-2 flex flex-col justify-center items-center'>
                     <p className='font-hero text-[8rem] text-white drop-shadow-xl'>Oceans</p>
                     <p className='font-hero text-[4rem] text-white drop-shadow-xl'>of</p>
                     <p className='font-hero text-[8rem] text-white drop-shadow-xl'>Terror</p>
                 </div>
-                <div className='text-center p-2 border rounded-lg shadow-lg flex flex-col justify-center items-center'>
+                <div className='text-center p-2 flex flex-col justify-center items-center'>
                     <p className='text-white font-hero text-[2rem]'>Oceans of Terra is an underwater adventure game full of magic and wonder. Defend the underwater World against the evil Mermaid Princess, Delila and you shall have life long glory!</p>
                 </div>
             </div>
