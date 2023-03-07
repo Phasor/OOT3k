0ff180f39d2691fe503e0751fc46fd0499a2f403
ben <phas0ruk@gmail.com>
2023-03-06 20:48:08 -0800

    fixed story board on mobile

--- frontend/components/Lore.js
+++ frontend/components/Lore.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'intersection-observer'; // import the polyfill
 
 export default function Lore() {
@@ -28,22 +28,20 @@ export default function Lore() {
   }, []);
 
   return (
-    <>
-      <section id="lore" className='w-full min-h-screen bg-[#74acf7] relative flex justify-center items-center'>
-          <div class="custom-shape-divider-top-1677879891">
-              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                  <path d="M649.97 0L550.03 0 599.91 54.12 649.97 0z" class="shape-fill"></path>
-              </svg>
-          </div>
-          
-          <div className='max-w-6xl flex flex-col justify-center items-center my-11'>
-            <p className='text-5xl font-raleway font-semibold text-gray-100 my-10'>Once Upon a Time...</p>
-            <div className='h-[20rem] w-[20rem] bg-red-300 my-11 hide-left mr-[20rem] flex justify-center items-center text-2xl'>Story Board 1</div>
-            <div className='h-[20rem] w-[20rem] bg-red-400 my-11 hide-right ml-[20rem] flex justify-center items-center text-2xl'>Story Board 2</div>
-            <div className='h-[20rem] w-[20rem] bg-red-500 my-11 hide-left mr-[20rem] flex justify-center items-center text-2xl'>Story Board 3</div>
-            <div className='h-[20rem] w-[20rem] bg-red-600 my-11 hide-right ml-[20rem] flex justify-center items-center text-2xl'>Story Board 4</div>
-          </div>
-      </section>
-    </>
+    <section id="lore" className='w-full min-h-screen bg-[#74acf7] relative flex justify-center items-center'>
+        <div class="custom-shape-divider-top-1677879891">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M649.97 0L550.03 0 599.91 54.12 649.97 0z" class="shape-fill"></path>
+            </svg>
+        </div>
+        
+        <div className='max-w-6xl flex flex-col justify-center items-center my-11'>
+          <p className='text-3xl md:text-5xl font-raleway font-semibold text-gray-100 my-10'>Once Upon a Time...</p>
+          <div className='h-[20rem] w-[20rem] md:h-[40rem] md:w-[60rem] bg-red-300 my-11 hide-left mr-[4rem] md:mr-[20rem] flex justify-center items-center text-4xl'>Story Board 1</div>
+          <div className='h-[20rem] w-[20rem] md:h-[40rem] md:w-[60rem] bg-red-400 my-11 hide-right ml-[4rem] md:ml-[20rem] flex justify-center items-center text-4xl'>Story Board 2</div>
+          <div className='h-[20rem] w-[20rem] md:h-[40rem] md:w-[60rem] bg-red-500 my-11 hide-left mr-[4rem] md:mr-[20rem] flex justify-center items-center text-4xl'>Story Board 3</div>
+          <div className='h-[20rem] w-[20rem] md:h-[40rem] md:w-[60rem] bg-red-600 my-11 hide-right ml-[4rem] md:ml-[20rem] flex justify-center items-center text-4xl'>Story Board 4</div>
+        </div>
+    </section>
   )
 }
