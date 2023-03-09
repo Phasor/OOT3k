bb3978d231649e25580626bb50dadb2b739608fb
ben <phas0ruk@gmail.com>
2023-03-08 23:03:33 -0800

    adds minting box

--- frontend/pages/mint.js
+++ frontend/pages/mint.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 export default function Mint() {
+  const [mintAmount, setMintAmount] = useState(1)
+  const [error, setError] = useState(null)
+
+  const handleIncrement = () => {
+    if(mintAmount >= 10) {
+      setError("Like your style, but it's a max mint of 10 at a time :(")
+      return
+    } else {
+      setMintAmount(prev => prev + 1)
+    }
+  }
+
+  const handleDecrement = () => {
+    setMintAmount(prev => prev - 1)
+  }
+
   return (
     <div className='w-screen h-screen'>
       <div className='w-full flex justify-center items-center'>
@@ -42,7 +58,30 @@ export default function Mint() {
                 className="border-2 border-gray-500 rounded-sm"
             />
             <p className='font-singleDay text-2xl mt-10'>How many Terra's do you want to mint?</p>
+
+            <div className='flex justify-between w-[20%] mt-10'>
+
+              <div className='w-[50%] border-2 border-gray-800 p-2 mr-2 flex justify-center items-center space-x-8'>
+                <div className="h-6 w-6 bg-gray-800 rounded-full flex justify-center  items-center cursor-pointer hover:scale-105">
+                  <p onClick={handleDecrement} className="text-gray-100 text-2xl mb-2">-</p>
+                </div>
+                <p className='text-2xl font-singleDay'>{mintAmount}</p>
+                <div className="h-6 w-6 bg-gray-800 rounded-full flex justify-center items-center cursor-pointer hover:scale-105">
+                  <p onClick={handleIncrement} className="text-gray-100 text-2xl mb-1">+</p>
+                </div>
+
+
+              </div>
+
+              <div className='w-[50%] bg-gray-800 ml-2 hover:scale-105 cursor-pointer flex justify-center items-center'>
+                <p className='font-singleDay text-2xl text-center text-white py-1 px-3'>MINT</p>
+              </div>
+
+            </div>
+              {error && <p className='font-singleDay text-xl mt-2'>{error}</p>}
         </div>
+
+
     </div>
   )
 }
