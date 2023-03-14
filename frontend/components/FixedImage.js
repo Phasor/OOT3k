1c36cd2e71caab949f4ee77ea4b56425edc4884c
ben <phas0ruk@gmail.com>
2023-03-14 13:37:29 -0700

    adds mint button to fixed image

--- frontend/components/FixedImage.js
+++ frontend/components/FixedImage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function FixedImage() {
   return (
   <div className='h-[700px] w-screen bg-[#74acf7] relative'>
-    <h3 className='text-[6rem] font-lucky text-gray-100 absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2'>Danger is Brewing</h3>
-    <p className='text-2xl font-leckton text-gray-100 absolute top-[70%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>Something was brewing many moons away as if someone was analyzing earth's movements day by day. What was on the horizon? Seemingly, no one knew. Yet, our heroes could feel danger brewing… Can you?</p>
+    <h3 className='text-[6rem] font-leckton font-bold text-gray-100 absolute top-[40%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>Something is Stiring</h3>
+    <p className='text-2xl font-leckton text-gray-100 absolute top-[60%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>Something was brewing many moons away as if someone was analyzing earth's movements day by day. What was on the horizon? Seemingly, no one knew. Yet, our heroes could feel something exciting about to happen. Can you?</p>
+    <div className='border rounded-full bg-blue-500 hover:bg-blue-600 cursor-pointer hover:scale-105 text-white font-leckton text-2xl py-2 px-8 absolute top-[80%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'><Link href="/mint" >Mint Your Terras</Link></div>
     <div className="bg-[url('/OOT-many-moons.jpeg')] h-full bg-center bg-cover bg-fixed"></div>
   </div>
 
