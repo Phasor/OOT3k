8f5bf4c457e934a89c08aa90b32349d362372664
ben <phas0ruk@gmail.com>
2023-03-16 10:00:01 -0700

    changes success nft card to pop out animation instead of 3d card

--- frontend/components/NFTCard.js
+++ frontend/components/NFTCard.js
@@ -1,47 +1,24 @@
 import React from 'react';
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function NFTCard({ url }) {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const rotateX = useTransform(y, [-100, 100], [30, -30]);
-  const rotateY = useTransform(x, [-100, 100], [-30, 30]);
 
   return (
     <div 
         style={{ width: '100%', perspective: 2000, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
-
         <motion.div
-        style={{
-            width: '285px',
-            height: '400px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '25px',
-            boxShadow: '0 2px 7px 1px rgba(31, 31, 31, 0.2)',
-            backgroundColor: '#1d1f21',
-            color: '#fff',
-            position: 'relative',
-            cursor: 'grab',
-            transform: 'translate3d(0, 0, 0)',
-            transformStyle: 'preserve-3d',
-            transformOrigin: '50% 50%',
-            rotateX: rotateX,
-            rotateY: rotateY,
-            x: x,
-            y: y,
-            z: 100,
-        }}
-        drag
-        dragElastic={0.16}
-        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-        whileTap={{ cursor: 'grabbing' }}
+          className="box"
+          initial={{ opacity: 0, scale: 0.1 }}
+          animate={{ opacity: 1, scale: [0.5, 1.8, 1] }}
+          transition={{
+            duration: 1.5,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
         >
-        <img className='p-4 object-cover' src={url} height={350} width={350} />
+          <img className='object-cover border-2 border-gray-600 rounded shadow-2xl' src={url} height={350} width={350} />
         </motion.div>
+
     </div>
   );
 }
