368f065378831c440e460dee4297d5db6de31cd8
ben <phas0ruk@gmail.com>
2023-03-22 21:21:52 -0700

    fixes image cutoff in header

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-        <div className="w-full min-h-screen flex justify-center items-center relative bg-[url('/Muacademy.png')] bg-center bg-cover bg-no-repeat mt-[70px] ">
+        <div className="w-full min-h-screen flex justify-center items-center relative bg-[url('/Muacademy.png')] bg-top bg-cover bg-no-repeat mt-[70px] h-[110vh]">
             <div className='text-center '>
                 {/* <p className='font-leckton font-bold text-[18vw] md:text-[16vw] lg:text-[12vw] xl:text-[8vw] text-gray-200 drop-shadow-xl'>Oceans</p>
                 <p className='font-leckton font-bold text-[12vw] md:text-[10vw] lg:text-[8vw] xl:text-[4vw] text-gray-200 drop-shadow-xl'>of</p>
