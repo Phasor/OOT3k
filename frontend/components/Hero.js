263355e708f802e6dae43423f641ad1216fb0ea0
ben <phas0ruk@gmail.com>
2023-03-23 21:43:10 -0700

    changes background image for next image component for main hero image

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -3,14 +3,22 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-        <div className="w-full h-[110vh] flex justify-center items-center relative bg-[url('/Muacademy.png')] bg-top bg-cover bg-no-repeat mt-[70px]">
-            <div className='text-center '>
-                {/* <p className='font-leckton font-bold text-[18vw] md:text-[16vw] lg:text-[12vw] xl:text-[8vw] text-gray-200 drop-shadow-xl'>Oceans</p>
-                <p className='font-leckton font-bold text-[12vw] md:text-[10vw] lg:text-[8vw] xl:text-[4vw] text-gray-200 drop-shadow-xl'>of</p>
-                <p className='font-leckton font-bold text-[18vw] md:text-[16vw] lg:text-[12vw] xl:text-[8vw] text-gray-200 drop-shadow-xl'>Terra</p> */}
-            </div>
-
-  
+        <div className="w-full h-[110vh] flex justify-center items-center  mt-[70px]">
+          <Image
+            src="/Muacademy.png"
+            alt="Picture of the Mu Academy, underwater sunken ship scene."
+            height={1500}
+            width={1500}
+            quality={90}
+            className="object-cover h-full w-full"
+          />
         </div>
   )
 }
+
+// export default function Hero() {
+//   return (
+//         <div className="w-full h-[110vh] flex justify-center items-center relative bg-[url('/Muacademy.png')] bg-top bg-cover bg-no-repeat mt-[70px]">
+//         </div>
+//   )
+// }
