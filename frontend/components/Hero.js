bb1131e6f2420cf66edc68ef69dff72f61c3d5cb
ben <phas0ruk@gmail.com>
2023-03-23 22:54:58 -0700

    changes background hero image scaling on mobile

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,24 +1,43 @@
 import React from 'react'
 import Image from 'next/image'
 
+function isMobile() {
+  if (typeof window !== "undefined") {
+    return window.innerWidth < 640;
+  }
+  return false;
+}
+
+
 export default function Hero() {
+
+  const mobile = isMobile();
+
   return (
-        <div className="w-full h-[110vh] flex justify-center items-center  mt-[70px]">
-          <Image
-            src="/Muacademy.png"
-            alt="Picture of the Mu Academy, underwater sunken ship scene."
-            height={1500}
-            width={1500}
-            quality={90}
-            className="object-cover h-full w-full"
-          />
-        </div>
+    <>
+      { mobile ? 
+        (
+            <div className="w-full h-[110vh] flex justify-center items-center relative bg-[url('/muCompressed.png')] bg-top bg-cover bg-no-repeat mt-[75px]"></div>
+        ) : 
+        
+        ( 
+          <div className="w-full h-[110vh] flex justify-center items-center mt-[80px] md:mt-[70px]">
+              <Image
+                src="/Muacademy.png"
+                alt="Picture of the Mu Academy, underwater sunken ship scene."
+                height={1500}
+                width={1500}
+                quality={90}
+          
+                placeholder="blur"
+                blurDataURL="/muLoading.jpeg"
+                className="object-cover h-full w-full"
+              />
+            </div>
+        )
+      }
+    </>
   )
 }
 
-// export default function Hero() {
-//   return (
-//         <div className="w-full h-[110vh] flex justify-center items-center relative bg-[url('/Muacademy.png')] bg-top bg-cover bg-no-repeat mt-[70px]">
-//         </div>
-//   )
-// }
+
