e98ba0563f6d875e2cdc441341dfc055624080c6
ben <phas0ruk@gmail.com>
2023-03-28 16:19:52 -0700

    removes yellow boxes for styling

--- frontend/components/Collection.js
+++ frontend/components/Collection.js
@@ -5,7 +5,7 @@ export default function Collection() {
 
 
   return (
-    <section id="collection" className='w-full min-h-screen bg-bg-blue flex flex-col justify-center items-center text-center border-2 border-yellow-500'>
+    <section id="collection" className='w-full  bg-bg-blue flex flex-col items-center text-center'>
        <p className='px-1 text-3xl text-gray-800 md:text-5xl font-raleway font-bold md:my-2 mt-10'>Playable collectible avatars living on the</p>
        <p className='p-1 text-3xl text-gray-800 md:text-5xl font-raleway font-bold md:my-2'>Ethereum blockchain</p>
        <p className='p-2 text-xl text-gray-800  px-4 text-center md:text-2xl font-raleway font-normal mt-6 '>3,600 cute NFTs contrived from over 140 unique, hand drawn traits with varying rarity.</p>
