4ec61429cacf1513063cc3d31eddde58b193203a
ben <phas0ruk@gmail.com>
2023-03-29 11:07:55 -0700

    fixes the issue of not being able to use nav links part way down the page

--- frontend/components/ScrollLink.js
+++ frontend/components/ScrollLink.js
@@ -1,24 +1,48 @@
-// components/ScrollLink.tsx
-import Link, { LinkProps } from "next/link";
-import React, { PropsWithChildren } from "react";
-// mirror the props of next/link component
+// // components/ScrollLink.tsx
+// import Link, { LinkProps } from "next/link";
+// import React, { PropsWithChildren } from "react";
+// // mirror the props of next/link component
 
 
-const ScrollLink = ({ children, ...props }) => {
+// const ScrollLink = ({ children, ...props }) => {
+//   const handleScroll = (e) => {
+//     e.preventDefault();
+//     //remove everything before the hash
+//     const targetId = e.currentTarget.href.replace(/.*\#/, "");
+//     const elem = document.getElementById(targetId);
+//     window.scrollTo({
+//       top: elem?.getBoundingClientRect().top,
+//       behavior: "smooth",
+//     });
+//   };
+//   return (
+//     <Link {...props} onClick={handleScroll}>
+//       {children}
+//     </Link>
+//   );
+// };
+// export default ScrollLink;
+
+import React from "react";
+
+const ScrollLink = ({ href, children }) => {
   const handleScroll = (e) => {
     e.preventDefault();
-    //remove everything before the hash
-    const targetId = e.currentTarget.href.replace(/.*\#/, "");
+    const targetId = href.replace(/.*\#/, "");
     const elem = document.getElementById(targetId);
+    const offset = elem.getBoundingClientRect().top + window.pageYOffset;
     window.scrollTo({
-      top: elem?.getBoundingClientRect().top,
+      top: offset,
       behavior: "smooth",
     });
   };
+
   return (
-    <Link {...props} onClick={handleScroll}>
+    <a href={href} onClick={handleScroll}>
       {children}
-    </Link>
+    </a>
   );
 };
+
 export default ScrollLink;
+
