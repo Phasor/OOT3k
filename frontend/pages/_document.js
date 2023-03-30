f695eff7375d0985c92464232c9d3661d55501e8
ben <phas0ruk@gmail.com>
2023-03-30 11:27:26 -0700

    adds favicons

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -10,7 +10,13 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700;800&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;800&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Lekton:wght@400;700&display=swap" rel="stylesheet"></link>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
       </Head>
       <body>
         <Main />
