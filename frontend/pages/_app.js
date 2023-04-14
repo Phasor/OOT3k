dba87d5c4567207881834f44499577ab2b8e6403
ben <phas0ruk@gmail.com>
2023-04-14 12:23:27 -0700

    adds mainnet custom RPC settings to thirdweb provider. You can now uncomment the mainnet section when wanting to mint on mainnet

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,19 +7,17 @@ function MyApp({ Component, pageProps }) {
 	const router = useRouter();
 	const INFURA_RPC = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
-	// activeChain can be "mumbai" or "ethereum"
-
 	return (
 		<ThirdwebProvider 
-			// activeChain="mumbai"
+		    // *** UNCOMMENT BELOW FOR MUMBAI ***
 			activeChain={{
 				// === Required information for connecting to the network === \\
 				chainId: 80001, // Chain ID of the network
 
 				// Array of RPC URLs to use
 				rpc: [
-					`https://polygon-mumbai.infura.io/v3/${INFURA_RPC}`, 			// MY RPC key
 					"https://mumbai.rpc.thirdweb.com",	 							// ThirdWeb RPC
+					`https://polygon-mumbai.infura.io/v3/${INFURA_RPC}`, 			// MY RPC key
 				],
 		
 				// === Information for adding the network to your wallet (how it will appear for first time users) === \\
@@ -27,7 +25,7 @@ function MyApp({ Component, pageProps }) {
 				nativeCurrency: {
 				  decimals: 18,
 				  name: "MATIC",
-				  symbol: "MATIC",
+				  symbol: "MATIC"
 				},
 				shortName: "maticmum", // Display value shown in the wallet UI
 				slug: "mumbai", // Display value shown in the wallet UI
@@ -41,6 +39,39 @@ function MyApp({ Component, pageProps }) {
 					"format": "png"
 				  },
 			  }}		
+
+			// *** UNCOMMENT BELOW FOR ETH MAINNET ***
+			// Reference values here: https://github.com/thirdweb-dev/js/blob/main/packages/chains/chains/1.ts
+			
+			// activeChain={{
+			// 	// === Required information for connecting to the network === \\
+			// 	chainId: 1, // Chain ID of the network
+
+			// 	// Array of RPC URLs to use
+			// 	rpc: [
+			// 		`https://mainnet.infura.io/v3/${INFURA_RPC}`, 					// MY RPC key
+			// 		"https://ethereum.rpc.thirdweb.com",	 						// ThirdWeb RPC
+			// 	],
+		
+			// 	// === Information for adding the network to your wallet (how it will appear for first time users) === \\
+			// 	// Information about the chains native currency (i.e. the currency that is used to pay for gas)
+			// 	nativeCurrency: {
+			// 	  decimals: 18,
+			// 	  name: "Ether",
+			// 	  symbol: "ETH"
+			// 	},
+			// 	shortName: "eth", // Display value shown in the wallet UI
+			// 	slug: "ethereum", // Display value shown in the wallet UI
+			// 	testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+			// 	chain: "ETH", // Name of the network
+			// 	name: "Ethereum Mainnet", // Name of the network
+			// 	icon: {
+			// 		"url": "ipfs://QmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9/ethereum/512.png",
+			// 		"height": 512,
+			// 		"width": 512,
+			// 		"format": "png"
+			// 	  },
+			//   }}		
 		>
 
 
